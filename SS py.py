0a7b1758ac31a508172aefddc1fea32f5670f92f
sean whelan <michael.h.whelan93@gmail.com>
2018-11-17 13:30:09 +0000

    Added a broken email client. Error with encoding.

--- SS py.py
+++ SS py.py
@@ -7,9 +7,11 @@ import re
 import random
 import copy
 from random import shuffle
-
-
-
+import smtplib
+from string import Template
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.header import Header
 
 class Person:
 	def __init__(self, name, email):
@@ -87,6 +89,8 @@ def main():
 			#	import pdb; pdb.set_trace()
 		else:
 			success=True
+	for user in usersG:
+		print(user.name, user.chosen.name)
 	print('success:', success)
 	print('fails:', fails)
 
@@ -105,6 +109,46 @@ def loopUsers():
 		#import pdb; pdb.set_trace()
 		return False
 		
-		
+def sendMessage(me, you):
+	print('sending.......')
+	#msg = EmailMessage()
+	s = smtplib.SMTP.connect(host='smtp.gmail.com',port='587')
+	s.starttls()
+	s.login('santysecrets@gmail.com', 'blitzen123')
+
+	msg = MIMEMultipart() 
+	#msg.set_content('sean is a dick')
+	message='sean is a dick'
+	msg['Subject'] = 'test mail'
+	msg['From'] = me
+	msg['To'] = you
+	#msg.attach(MIMEText(message, 'plain'))
+	msg = MIMEText(message, 'plain', charset)
+	#s.send_message(msg)
+	s.quit()
+
+def sendtest():
+	####smtp_host = 'smtp.live.com'        # microsoft
+	smtp_host = 'smtp.gmail.com'       # google
+	#smtp_host = 'smtp.mail.yahoo.com'  # yahoo
+	login, password = 'santysecrets@gmail.com', 'blitzen123'
+	recipients_emails = ['seanwhelan117@gmail']
+
+	msg = MIMEText('sean is a gobshite', 'plain', 'utf-8')
+	msg['Subject'] = Header('test', 'utf-8')
+	msg['From'] = login
+	msg['To'] = 'seanwhelan117@gmail'
+
+	s = smtplib.SMTP(smtp_host, 587, timeout=10)
+	s.set_debuglevel(1)
+	try:
+		s.starttls()
+		s.login(login, password)
+		s.sendmail(msg['From'], recipients_emails, msg.as_string())
+	finally:
+		s.quit()
+
 if __name__ == "__main__":
-	main()
+	#main()
+	#sendMessage('santysecrets@gmail.com','seanwhelan117@gmail.com')
+	sendtest()
