a9c2667d7baafa2f21e66bcc92eafcd9f5ba4dab
Michael <michael@mr.pink>
2019-02-18 09:57:14 +0000

    -Working change detection -If the user starts changing the names/email of people in a group then after a second or on ENTER the changes is triggered -The user can add blank (new) people

--- secret-santa/src/MainDetails/Details.js
+++ secret-santa/src/MainDetails/Details.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import "../App.css";
 import Button from '@material-ui/core/Button';
 
+const WAIT_INTERVAL = 1000;
+const ENTER_KEY = 13;
+
 export default class GroupDetails extends Component {
 	constructor(props) {
 		super(props);
@@ -10,9 +13,25 @@ export default class GroupDetails extends Component {
 	}
 
 	handleChange = event => {
-		this.setState({
+		console.log(event.target.key,event.target.value);
+		clearTimeout(this.timer);
+		this.timer = setTimeout(this.triggerChange, WAIT_INTERVAL);
+		/*this.setState({
 			[event.target.type]: event.target.value
-		});
+		});*/
+	}
+
+	handleKeyDown = e => {
+		if (e.keyCode === ENTER_KEY) {
+			clearTimeout(this.timer);
+			this.triggerChange();
+		}
+	}
+
+	triggerChange = () => {
+		console.log("trigger");
+		/*const { value } = this.state;
+		this.props.onChange(value);*/
 	}
 
 	componentWillReceiveProps(nextProps) {
@@ -28,8 +47,30 @@ export default class GroupDetails extends Component {
 
 	showAlert = (msg) =>{
 		alert(msg);
+		console.log(this.people);
 	}
 
+	addPerson = (single) =>{
+		var addDirect = false;
+		if (single === null){
+			single = {"name":"","email":"","person_id":"new"};
+			addDirect = true;
+		}
+		var person = <div className="person">
+				<Button variant="outlined" color="primary"
+				onClick={this.showAlert.bind(this,"Are you sure you want to delete: "+single.name)}>-</Button>
+				<input type="text" key={"name-"+single.person_id} defaultValue={single.name}
+					onChange ={this.handleChange}  onKeyDown={this.handleKeyDown}/>
+				<input type="text" key={"email-"+single.person_id} defaultValue={single.email}/>
+				<Button variant="outlined" color="primary"
+				onClick={this.showAlert.bind(this,"You are adding a not field")}>Nots</Button>
+			</div>
+
+		if(!addDirect){return person;}
+		var newGroup = this.state.activeGroup;
+		newGroup.people.push(person);
+		this.setState({activeGroup: newGroup});
+	}
 
 	generatePeople = () =>{
 		var list =[];
@@ -40,15 +81,7 @@ export default class GroupDetails extends Component {
 		var peopleList = this.state.activeGroup.people;
 		for(var i =0; i < peopleList.length;++i){
 			var single = peopleList[i];
-			var person = <div className="person">
-				<Button variant="outlined" color="primary"
-				onClick={this.showAlert.bind(this,"Are you sure you want to delete: "+single.name)}>-</Button>
-				<input type="text" key={"name-"+single.person_id} defaultValue={single.name}/>
-				<input type="text" key={"email-"+single.person_id} defaultValue={single.email}/>
-				<Button variant="outlined" color="primary"
-				onClick={this.showAlert.bind(this,"You are adding a not field")}>Nots</Button>
-			</div>
-			list.push(person);
+			list.push(this.addPerson(single));
 		}
 		return list;
 	}
@@ -68,6 +101,8 @@ export default class GroupDetails extends Component {
 				<>
 				<h1>{group.group_name}</h1>
 				{this.people}
+				<Button variant="outlined" color="primary"
+					onClick={this.addPerson.bind(this,null)}>+</Button>
 				</>
 			):(
 				<p> Nothing to see here</p>
