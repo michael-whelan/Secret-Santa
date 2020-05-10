73b0bb39702a9c5af7c975b8bbdedb92fae977d8
michael-whelan <seagullmania93@gmail.com>
2020-05-10 17:02:53 +0100

    F_Auth: Updated Routes. Added home

---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./Home.css";
+
+const Home = () => {
+	return (
+		<div className="home">
+			<div className="column">
+			</div>
+			<div className="column"></div>
+			<div className="column"></div>
+		</div>
+	);
+};
+
+export default Home;
