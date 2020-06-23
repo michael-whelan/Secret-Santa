9c302ae17c2b2b1fc56d2136d4b96a9357356a0f
michael-whelan <seagullmania93@gmail.com>
2020-06-23 16:24:46 +0100

    Changed to pointing at dev server

--- src/store/api/actions.js
+++ src/store/api/actions.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 import { GET_ERROR, POST_ERROR, PUT_ERROR, DELETE_ERROR } from "./types";
 
-const dom = "http://localhost:8080/";
-//const dom = "http://michaelwhelan.pythonanywhere.com/";
+//const dom = "http://localhost:8080/";
+const dom = "http://michaelwhelan.pythonanywhere.com/";
 
 
 const getError = (message) => ({
